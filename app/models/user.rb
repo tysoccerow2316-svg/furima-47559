a79d808566f416b7211a51d808ae26cba21eca64
tysoccerow2316-svg <ty.soccer.ow.2316@gmail.com>
2026-03-07 10:33:46 +0900

    バリデーションコミット

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true,
+                    format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i, message: 'must be a valid email address' }
+  validates :password, presence: true, length: { minimum: 6 },
+                       format: { with: /\A(?=.*[a-zA-Z])(?=.*[0-9]).{6,}\z/, message: 'must include both letters and numbers' }
+  validates :password, confirmation: true
+  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
+  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー]+\z/ }
+  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー]+\z/ }
+  validates :date_of_birth, presence: true
 end
